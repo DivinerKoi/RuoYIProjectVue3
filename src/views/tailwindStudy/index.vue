e51618f42ee785c34d484eaf534bf1175b2ad8cb
DivinerKoi <you@example.com>
2024-08-29 22:36:35 +0800

    初始化

---
+++ src/views/tailwindStudy/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="text-white p-4 rounded-lg bg-green-200">
+      Hello, Tailwind CSS with Vue 3!
+    </div>
+    <h1 class="text-3xl">
+      Hello world!
+    </h1>
+    <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+      <div class="shrink-0">
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-slate-500 bg-blend-color bg-amber-800">You have a new message!</p>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
